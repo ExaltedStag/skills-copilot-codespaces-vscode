3f3c2303baff0ad6892e291c9eb698b1293a4d06
Marko Prusac <markoprusac@gmail.com>
2026-04-30 07:37:07 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,19 @@
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create comments array
+let comments = [];
+
+// Create endpoint to get comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// Create endpoint to add comment
+app.post('/comments', (req, res) => {
+  const comment = req.body.comment;
+  comments.push(comment);
+  res.json({ message: 'Comment added successfully' });
+});
